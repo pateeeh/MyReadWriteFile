c349ba5a06fe1817ba0b710e12240a8b62de595d
pateeh <fatihazzam33@gmail.com>
2025-02-27 13:13:18 +0700

    .

--- app/src/main/java/com/example/myreadwritefile/MainActivity.kt
+++ app/src/main/java/com/example/myreadwritefile/MainActivity.kt
@@ -25,26 +25,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonSave.setOnClickListener(this)
     }
 
+    override fun onClick(view: View){
+        when(view.id){
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
     private fun newFile() {
         binding.editTitle.setText("")
         binding.editFile.setText("")
         Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onClick(view: View){
-        when(view.id){
-            R.id.button_new -> newFile()
-            R.id.button_open -> showList()
-            R.id.button_save ->
-        }
-    }
-
     private fun showList() {
-        val items = fileList()
+        val items = fileList().filter { fileName -> (fileName != "profileInstalled") }.toTypedArray()
+
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Pilih File yang Di Inginkan")
-        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }}
-
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        val alert = builder.create()
+        alert.show()
+    }
     private fun loadData(title: String) {
         val fileModel = FileHelper.readFromFile(this,title)
         binding.editTitle.setText(fileModel.filename)
@@ -56,6 +59,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when{
             binding.editTitle.text.toString().isEmpty() ->
                 Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() ->
+                Toast.makeText(this, "Konten harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            else ->{
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
